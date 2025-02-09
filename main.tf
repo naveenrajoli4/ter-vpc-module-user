b390613d1ff63a89458e26ebf0bb15aa6017aa96
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-02-09 18:20:27 +0530

    ij

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 module "vpc" {
-    source = "../ter-vpc-module-dev"
-    location = var.location
-    project_name = var.project_name
-    environment = var.environment
-    cidr_block = var.cidr_block
-    commn_tags = var.commn_tags
-    vpc_tags = var.vpc_tags
-    igw_tags = var.igw_tags
-    public_subnet_cidr = var.public_subnet_cidr
-    public_subet_tags = var.public_subet_tags
-    private_subnet_cidr = var.private_subnet_cidr
-    private_subnet_tags = var.private_subnet_tags
-    database_subnet_cidr = var.database_subnet_cidr
-    database_subnet_tags = var.database_subnet_tags
-    public_route_table_tags = var.public_route_table_tags
-    private_route_table_tags = var.private_route_table_tags
-    database_route_table_tags = var.database_route_table_tags
-    is_peering_required = true # This is for peering b/w 2 vpc's
+  source                    = "../ter-vpc-module-dev"
+  location                  = var.location
+  project_name              = var.project_name
+  environment               = var.environment
+  cidr_block                = var.cidr_block
+  commn_tags                = var.commn_tags
+  vpc_tags                  = var.vpc_tags
+  igw_tags                  = var.igw_tags
+  public_subnet_cidr        = var.public_subnet_cidr
+  public_subet_tags         = var.public_subet_tags
+  private_subnet_cidr       = var.private_subnet_cidr
+  private_subnet_tags       = var.private_subnet_tags
+  database_subnet_cidr      = var.database_subnet_cidr
+  database_subnet_tags      = var.database_subnet_tags
+  public_route_table_tags   = var.public_route_table_tags
+  private_route_table_tags  = var.private_route_table_tags
+  database_route_table_tags = var.database_route_table_tags
+  is_peering_required       = true # This is for peering b/w 2 vpc's
 
-    }
+}
--- variable.tf
+++ variable.tf
@@ -1,84 +1,84 @@
 variable "location" {
-    default = "kdp"
+  default = "kdp"
 }
 
 variable "project_name" {
-    default = "expense"
+  default = "expense"
 }
 
 variable "environment" {
-    default = "prod"
+  default = "prod"
 }
 
 variable "cidr_block" {
-    default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "commn_tags" {
-    default = {
-        Owner = "Naveen Rajoli"
-        project = "expense-app"
-        Location = "kadapa-AP"
-        Environment = "production"
-    }
+  default = {
+    Owner       = "Naveen Rajoli"
+    project     = "expense-app"
+    Location    = "kadapa-AP"
+    Environment = "production"
+  }
 }
 
 variable "vpc_tags" {
-    default = {
-        Purpose = "VPC-creation-for-expense"
-        Reason = "For-security"
-    }
+  default = {
+    Purpose = "VPC-creation-for-expense"
+    Reason  = "For-security"
+  }
 }
 
 variable "igw_tags" {
-     default = {
-        Purpose = "IGW-creation-for-expense"
-        Reason = "For-security"
-    }
+  default = {
+    Purpose = "IGW-creation-for-expense"
+    Reason  = "For-security"
+  }
 }
 
 variable "public_subnet_cidr" {
-    default = ["10.0.1.0/24" , "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 variable "public_subet_tags" {
-    default = {
-        Purpose = "public-subet-creation-for-expense"
-    }
+  default = {
+    Purpose = "public-subet-creation-for-expense"
+  }
 }
 
 variable "private_subnet_cidr" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 variable "private_subnet_tags" {
-    default = {
-        Purpose = "private-subet-creation-for-expense"
-    }
+  default = {
+    Purpose = "private-subet-creation-for-expense"
+  }
 }
 
 variable "database_subnet_cidr" {
-    default = ["10.0.21.0/24", "10.0.22.0/24"]
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 variable "database_subnet_tags" {
-    default = {
-        Purpose = "database-subet-creation-for-expense"
-    }
+  default = {
+    Purpose = "database-subet-creation-for-expense"
+  }
 }
 
- variable "public_route_table_tags" {
-      default = {
-        Purpose = "public-route-table-creation-for-expense"
-    }
- }
+variable "public_route_table_tags" {
+  default = {
+    Purpose = "public-route-table-creation-for-expense"
+  }
+}
 
- variable "private_route_table_tags" {
-      default = {
-        Purpose = "private-route-table-creation-for-expense"
-    }
- }
+variable "private_route_table_tags" {
+  default = {
+    Purpose = "private-route-table-creation-for-expense"
+  }
+}
 
-  variable "database_route_table_tags" {
-      default = {
-        Purpose = "database-route-table-creation-for-expense"
-    }
- }
- 
+variable "database_route_table_tags" {
+  default = {
+    Purpose = "database-route-table-creation-for-expense"
+  }
+}
+
